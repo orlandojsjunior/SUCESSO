359dbe8d444b93dcdc377c925e7cf9fb5e9fe0b1
Orlando Junior <orlandojsjunior@hotmail.com>
2023-12-04 22:11:51 -0300

    Código com a receita para o sucesso

---
+++ receita_sucesso.py
@@ -0,0 +1,11 @@
+def success(dedication, persistence, passion):
+    dedication += 1    # dedicar-se
+    persistence += 1   # ser persistente
+    passion = True     # Paixão verdadeira - (Um sentimento intenso por um lugar seguro, tranquilo e confortável.)
+    
+    if passion == True:
+        magic = dedication + persistence
+        return magic
+    else: 
+        magic = 0
+        return magic
