19eb140528a544156dbcf650b2c900dcb73ca4d4
Orlando Junior <134120820+orlandojsjunior@users.noreply.github.com>
2024-04-23 22:48:43 -0300

    Update receita_sucesso.py

--- receita_sucesso.py
+++ receita_sucesso.py
@@ -1,12 +1,13 @@
 def success(dedication, persistence, passion):
     dedication += 1    # Dedicação: Aumente sua dedicação em pelo menos um ponto.
     persistence += 1   # Persistência: Seja um pouco mais persistente a cada dia.
-    passion = True     # Paixão Verdadeira: Encontre e mantenha a verdadeira paixão no que faz.
+    passion = True     # Paixão Verdadeira: Encontre e mantenha a verdadeira paixão no que faz. 
     
     if passion == True:
         magic = dedication + persistence
-        return magic # Com esses ingredientes, você pode criar sua própria magia do sucesso! Lembre-se, a paixão é a chave para ativar a fórmula.
+        return magic # Com esses ingredientes, você pode criar sua própria magia do sucesso! 
     else: 
         magic = 0
         return magic
 
+# Lembre-se, a paixão é a chave para ativar a fórmula.
